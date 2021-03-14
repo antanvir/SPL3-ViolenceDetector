3ba52471c8d0e6a7d15e2e3d7998e7a26200655d
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2021-03-15 01:50:54 +0600

    file post

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import YouTubeIcon from '@material-ui/icons/YouTube';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
+import axios from 'axios';
 import ReactPlayer from 'react-player';
 import React, { useState } from 'react';
-import captureVideoFrame from "capture-video-frame";
 
 
 const styles = {
@@ -18,108 +18,138 @@ const styles = {
 }
 
 function App() {
-  const [filepath, setVideoFileUrl] = useState("");
-  const [videoType, setVideoType] = useState("folder");
-  const [player, setPlayer] = useState(null);
+	const [filepath, setVideoFileUrl] = useState("");
+	const [videoType, setVideoType] = useState("folderFile");
+	const [player, setPlayer] = useState(null);
 
 
-  const onInputChangeHandler = event => {
-    if(videoType == "folder"){
-      const { files } = event.target;
-      console.log(files[0]);
-      
-      let reader = new FileReader();
-      reader.readAsDataURL(files[0]);
-      reader.onload = (event) => {
-        // console.log(event.target.result);
-        setVideoFileUrl(event.target.result);
-      }
-    }else{
-      // console.log(ReactPlayer.canPlay(event.target.value));
-      let videoPlayable = ReactPlayer.canPlay(event.target.value);
-      if (videoPlayable) {   
-        setVideoFileUrl(event.target.value);
-      }
-    }
-    
-  }
+	const onInputChangeHandler = event => {
+		let post_data = {
+			videoType: "",
+			videoPath: "",
+			video: null
+		}
+		if(videoType == "folderFile"){
+			const { files } = event.target;
+			console.log(files[0]);
+			
+			let reader = new FileReader();
+			reader.readAsDataURL(files[0]);
+			reader.onload = (event) => {
+				// console.log(event.target.result);
+				setVideoFileUrl(event.target.result);
 
-  const handleVideoTypeChange = (event, currentVideoType) => {
-    if(currentVideoType == "youtube"){
-      document.getElementById("youtube").style.color = "blue";
-      document.getElementById("folder").style.color = "gray";
-      document.getElementById("standard-basic").type = "text";
-      document.getElementById("standard-basic").placeholder = "Paste youtube video url";
-    }else{
-      document.getElementById("folder").style.color = "blue";
-      document.getElementById("youtube").style.color = "gray";
-      document.getElementById("standard-basic").type = "file";
-    }
-    setVideoType(currentVideoType);
-  }
+				post_data["videoType"] = videoType;
+				post_data["videoPath"] = "";
+				post_data["video"] = event.target.result;
 
-  const onVideoBeingReadyToPlay = (event) => {
-    // When you want to record the image 
-    console.log(player.getInternalPlayer());
-    const frame = captureVideoFrame(player.getInternalPlayer(), "png");
-    console.log(frame);
-    // Show the image
-    const frameImage = document.getElementById("frame-image");
-    frameImage.setAttribute("src", frame.dataUri);
-  }
+				axios.post('http://localhost:5000/video', post_data)
+				.then((response) => {
+					console.log(response);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+			}
+		}
+		else{
+			// console.log(ReactPlayer.canPlay(event.target.value));
+			let videoPlayable = ReactPlayer.canPlay(event.target.value);
+			if (videoPlayable) {   
+				setVideoFileUrl(event.target.value);
+
+				post_data["videoType"] = videoType;
+				post_data["videoPath"] = event.target.value;
+				post_data["video"] = null;
+
+				axios.post('http://localhost:5000/video', post_data)
+				.then((response) => {
+					console.log(response);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+				// axios.post('http://localhost:5000/video', {
+				// 	videoType: videoType,
+				// 	videoPath: event.target.value,
+				// 	video: null
+				// })
+				// .then((response) => {
+				// 	console.log(response);
+				// })
+				// .catch((error) => {
+				// 	console.log(error);
+				// });
+			}
+		}
+
+		// axios.post('http://localhost:5000/video', post_data)
+		// 	.then((response) => {
+		// 		console.log(response);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.log(error);
+		// 	});
+		
+	}
+
+	const handleVideoTypeChange = (event, currentVideoType) => {
+		if(currentVideoType == "youtubeFile"){
+			document.getElementById("youtube").style.color = "blue";
+			document.getElementById("folder").style.color = "gray";
+			document.getElementById("standard-basic").type = "text";
+			document.getElementById("standard-basic").placeholder = "Paste youtube video url";
+		}else{
+			document.getElementById("folder").style.color = "blue";
+			document.getElementById("youtube").style.color = "gray";
+			document.getElementById("standard-basic").type = "file";
+		}
+		setVideoType(currentVideoType);
+	}
 
 
 
-  return (
-    <div className="App">
-      <div>
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" /> 
-            <b> ViolenceDetector </b>   
-        </header>
-        <hr/>
-      </div>
+	return (
+		<div className="App">
+		<div>
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" /> 
+				<b> ViolenceDetector </b>   
+			</header>
+			<hr/>
+		</div>
 
-      <div style={ {flexDirection: 'row'} }>
-        <b style={ {color: 'rgb(60, 70, 80)', paddingRight: '0.5em'} }>Paste Video URL From </b>
+		<div style={ {flexDirection: 'row'} }>
+			<b style={ {color: 'rgb(60, 70, 80)', paddingRight: '0.5em'} }>Paste Video URL From </b>
 
-        <ToggleButtonGroup size="small" value={videoType} exclusive onChange={handleVideoTypeChange} style={ {paddingRight: '0.6em'} }>
-          <ToggleButton value="folder">
-            <FolderIcon id="folder" fontSize="small" color="primary"/>
-          </ToggleButton>
-          <ToggleButton value="youtube">
-            <YouTubeIcon id="youtube" fontSize="small" color="action" hover="Youtube"/>
-          </ToggleButton>
-        </ToggleButtonGroup>
+			<ToggleButtonGroup size="small" value={videoType} exclusive onChange={handleVideoTypeChange} style={ {paddingRight: '0.6em'} }>
+			<ToggleButton value="folderFile">
+				<FolderIcon id="folder" fontSize="small" color="primary"/>
+			</ToggleButton>
+			<ToggleButton value="youtubeFile">
+				<YouTubeIcon id="youtube" fontSize="small" color="action" hover="Youtube"/>
+			</ToggleButton>
+			</ToggleButtonGroup>
 
-        <Input id="standard-basic" type="file" name="video" color="primary" onChange={onInputChangeHandler} />
-      </div>
+			<Input id="standard-basic" type="file" name="videoFile" color="primary" onChange={onInputChangeHandler} />
+		</div>
 
-      <div>
-        <ReactPlayer 
-          id="videoPlayer" 
-          style={ styles.videoPlayer } 
-          url={ filepath } 
-          controls={ true } 
-          ref={ player => { setPlayer(player); } }
-          config={{ 
-            file: { 
-              attributes: {
-                crossOrigin: 'anonymous'
-              }
-            }
-          }}
-          onPlay={ onVideoBeingReadyToPlay }
-        />
-      </div>
-      
+		<div>
+			<ReactPlayer 
+			id="videoPlayer" 
+			style={ styles.videoPlayer } 
+			url={ filepath } 
+			controls={ true } 
+			/>
+		</div>
+		
 
-      <div>
-        <img id="frame-image" value="check" />
-      </div>
+		<div>
+			<img id="frame-image" value="check" />
+		</div>
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
